2752f06906e90a72c73f3e57e1da667a8827f624
akshayaspradeep <akshaya.s@thinkpalm.com>
2023-11-21 17:38:01 +0530

    Initial Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,9 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+
+
+        
     }
 
     public IActionResult Index()
